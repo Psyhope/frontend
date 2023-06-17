e20e53b6876771c45fb25f395e504d3bc9c77ca8
iyoubee <al.ayubi2020@gmail.com>
2023-06-17 23:53:48 +0700

    feat: typewriter animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+'use client'
 import Image from 'next/image'
 import HeroAssets from '../../public/assets/Hero Asset.svg'
+import { TypeAnimation } from 'react-type-animation'
 
 export default function Home() {
   return (
@@ -10,13 +12,24 @@ export default function Home() {
           <p className="text-[#E9D7FE99] font-inter font-bold text-4xl">
             Layanan Konseling UI
           </p>
-          <p className="text-[#F2F4F7] text-sm mt-3 mb-14 leading-relaxed">
+          <p className="text-[#F2F4F7] text-sm mt-3 mb-10 leading-relaxed">
             Aliansi Departemen Adkesma BEM se-UI menyediakan layanan konseling
             sebaya yang diperuntukkan bagi mahasiswa program sarjana atau
             vokasi. Bersama teman sebaya, kamu dapat bercerita dengan aman dan
             nyaman.
           </p>
-          <button className="py-3 font-semibold rounded-lg drop-shadow-lg active:drop-shadow-none px-4 bg-[#7F56D9] flex-none w-fit text-white">
+          <div className="bg-[#D6BBFB] w-fit px-4 py-3 rounded-t-xl rounded-r-xl text-[#53389E] font-inter font-semibold">
+            <TypeAnimation
+              sequence={[
+                'Butuh teman bercerita? Yuk, curhat dengan konselor sebaya! 👇👇👇',
+                5000,
+                'Butuh teman bercerita? Yuk, curhat dengan konselor sebaya! ❤️❤️❤️',
+                5000,
+              ]}
+              repeat={Infinity}
+            />
+          </div>
+          <button className="mt-2 py-3 font-semibold rounded-lg drop-shadow-lg active:drop-shadow-none px-4 bg-[#7F56D9] flex-none w-fit text-white">
             Daftar Konseling Sekarang!
           </button>
         </div>
