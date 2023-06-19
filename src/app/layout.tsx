72a733f7069c62a03c5a322c090a7ebc089771cc
iyoubee <al.ayubi2020@gmail.com>
2023-06-19 20:15:30 +0700

    feat: done login page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
+'use client'
 import { AuthProvider } from '@/context/AuthContext'
 import './globals.css'
 import { Poppins } from 'next/font/google'
 import { GraphQLProvider } from '@/context/GraphQLContext'
 import RootStyleRegistry from '@/emotion'
 import { Navbar } from '@elements'
+import { usePathname } from 'next/navigation'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -20,13 +22,19 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const pathname = usePathname()
+
   return (
     <html lang="en">
       <body className={poppins.className}>
         <RootStyleRegistry>
           <AuthProvider>
             <GraphQLProvider>
-              <div className="bg-white min-h-screen text-black flex flex-col items-center justify-center">
+              <div
+                className={`${
+                  pathname == '/login' ? 'bg-[#E9D7FE]' : 'bg-white'
+                } min-h-screen text-black flex flex-col items-center justify-center`}
+              >
                 <Navbar />
                 <div className="max-w-[1920px] w-full">{children}</div>
               </div>
