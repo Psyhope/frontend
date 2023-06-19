546d020a8c9d6b0b31ae8e7cd309437d897247c0
yudiptr <94793172+yudiptr@users.noreply.github.com>
2023-06-19 20:05:37 +0700

    done onboarding minus styling

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { OnBoardingModule } from '@modules'
+
+export default function OnBoarding() {
+  return (
+      <main className="min-h-screen md:pt-5">
+        <OnBoardingModule></OnBoardingModule>
+      </main>
+    )
+}
---
+++ src/components/modules/OnboardingModule/interface.ts
@@ -0,0 +1,10 @@
+export interface OnBoardingFormInterface {
+    name : string
+    gender : "Laki-laki" | "Perempuan" | "Memilih tidak menjawab"
+    npm : string
+    prodi : "S-1 Reguler" | "S-1 Paralel" | "S-1 KKI" | "Vokasi" 
+    fakultas : string
+    jurusan : string
+    socmed : "line" | "instagram"
+    linkSocmed : string
+}
