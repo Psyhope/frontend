c3bd6f6464c71ccbea1fe654a795d6fbdeab157a
yudiptr <94793172+yudiptr@users.noreply.github.com>
2023-06-21 15:30:02 +0700

    Pretttier

--- src/components/modules/DashboardModule/index.tsx
+++ src/components/modules/DashboardModule/index.tsx
@@ -2,10 +2,5 @@ import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 
 export const DashboardModule: React.FC = () => {
-
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div></div>
 }
