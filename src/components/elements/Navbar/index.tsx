1e7b25f04f6ba1f91d98f836de864bca9f87e581
yudiptr <94793172+yudiptr@users.noreply.github.com>
2023-06-30 18:29:00 +0700

    modif navbar to match active pathname

--- src/components/elements/Navbar/index.tsx
+++ src/components/elements/Navbar/index.tsx
@@ -22,21 +22,21 @@ export const Navbar: React.FC = () => {
     <div
       className={`bg-[#FEFEF2] h-[84px] w-full drop-shadow-sm shadow-sm flex items-center justify-between px-10 sticky top-0 transition-all duration-1000 z-50 ${
         scroll ? 'md:top-1 md:scale-[0.99] lg:top-2' : 'top-0 scale-100'
-      } ${pathname == '/login' && 'hidden'}`}
+      } ${pathname == '/login' && 'hidden'}`} 
     >
       <div>
         <Psyhope />
       </div>
       <div className="items-center hidden gap-5 md:flex">
-        <button className="flex font-inter font-semibold gap-2 border-b-2 hover:border-[#026AA2] border-transparent px-4 transition-all flex-none text-[#0BA5EC] hover:text-[#026AA2]">
+        <button className={`flex font-inter font-semibold gap-2 border-b-2 hover:border-[#026AA2] ${pathname == '/' ? 'border-[#026AA2] text-[#026AA2]' : 'border-transparent'} px-4 transition-all flex-none text-[#0BA5EC] hover:text-[#026AA2]`}>
           <Home />
           Home
         </button>
-        <button className="flex font-inter font-semibold gap-2 border-b-2 hover:border-[#026AA2] border-transparent px-4 transition-all flex-none text-[#0BA5EC] hover:text-[#026AA2]">
+        <button className={`flex font-inter font-semibold gap-2 border-b-2 hover:border-[#026AA2] ${pathname == '/123' ? 'border-[#026AA2] text-[#026AA2]' : 'border-transparent'} px-4 transition-all flex-none text-[#0BA5EC] hover:text-[#026AA2]`}>
           <Event />
           Event
         </button>
-        <button className="flex font-inter font-semibold gap-2 border-b-2 hover:border-[#026AA2] border-transparent px-4 transition-all flex-none text-[#0BA5EC] hover:text-[#026AA2]">
+        <button className={`flex font-inter font-semibold gap-2 border-b-2 hover:border-[#026AA2] ${pathname == '/ghq' || pathname ==  '/schedule' ? 'border-[#026AA2] text-[#026AA2]' : 'border-transparent'} px-4 transition-all flex-none text-[#0BA5EC] hover:text-[#026AA2]`}>
           <Konseling />
           Konseling
         </button>
