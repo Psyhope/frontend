84c1ea133dc2dd60cec790de230a740c748baa00
Maurice Yang <mauriceyangmy28@gmail.com>
2023-07-10 13:43:48 +0700

    fix: Fixing broken codegen caused by invalid env

--- codegen.ts
+++ codegen.ts
@@ -1,8 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli'
-import { env } from './env.mjs'
 
 const config: CodegenConfig = {
-  schema: `${env.NEXT_PUBLIC_BACKEND_URL}/graphql`,
+  schema: `${process.env.NEXT_PUBLIC_BACKEND_URL}/graphql`,
   documents: ['src/**/*.ts?(x)'],
   generates: {
     './src/__generated__/': {
