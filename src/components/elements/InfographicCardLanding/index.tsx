ec2ae0805543be3706a8aa8b5f9c450c725f1558
iyoubee <al.ayubi2020@gmail.com>
2023-07-31 14:04:08 +0700

    fix: useEditor on infographic

--- src/components/elements/InfographicCardLanding/index.tsx
+++ src/components/elements/InfographicCardLanding/index.tsx
@@ -1,8 +1,17 @@
 import Image from 'next/image'
 import React from 'react'
-import PlaceHolder from '../../../../public/assets/Placeholder.png'
 import { InfographicCardLandingProps } from './interface'
-import Link from 'next/link'
+import NextLink from 'next/link'
+import { useEditor } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import Underline from '@tiptap/extension-underline'
+import Superscript from '@tiptap/extension-superscript'
+import Link from '@tiptap/extension-link'
+import Subscript from '@tiptap/extension-subscript'
+import Highlight from '@tiptap/extension-highlight'
+import TextStyle from '@tiptap/extension-text-style'
+import Color from '@tiptap/extension-color'
+import TextAlign from '@tiptap/extension-text-align'
 
 export const InfographicCardLanding: React.FC<InfographicCardLandingProps> = ({
   description,
@@ -10,8 +19,28 @@ export const InfographicCardLanding: React.FC<InfographicCardLandingProps> = ({
   infograficUrl,
   title,
 }) => {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Underline,
+      Link,
+      Superscript,
+      Subscript,
+      Highlight,
+      TextStyle,
+      Color,
+      TextAlign.configure({ types: ['heading', 'paragraph'] }),
+    ],
+    content: description,
+    editorProps: {
+      attributes: {
+        class: 'font-inter text-sm md:text-base',
+      },
+    },
+  })
+
   return (
-    <Link href={`/infographic/${id}`}>
+    <NextLink href={`/infographic/${id}`}>
       <div className="lg:w-[445px] w-[200px] rounded-lg relative flex-none pr-5 lg:my-10">
         <div className="w-full aspect-infografic relative">
           <Image
@@ -26,10 +55,10 @@ export const InfographicCardLanding: React.FC<InfographicCardLandingProps> = ({
             {title}
           </p>
           <p className="lg:max-h-20 max-h-12 pb-6 overflow-hidden lg:text-base text-xs">
-            {description}
+            {editor && editor.getText()}
           </p>
         </div>
       </div>
-    </Link>
+    </NextLink>
   )
 }
