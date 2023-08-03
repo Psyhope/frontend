23a860147cc345468c8577899a9515d303398cf7
Maurice Yang <mauriceyangmy28@gmail.com>
2023-08-03 20:20:27 +0700

    Saving before counselor page

--- src/app/counselor/[id]/page.tsx
+++ src/app/counselor/[id]/page.tsx
@@ -1,7 +1,11 @@
 'use client'
 
+import { GET_COUNSELOR } from '@/actions/counselor'
 import ClientTable from '@/components/elements/ClientTable'
+import { useQuery } from '@apollo/client'
+import { Badge, Button } from '@mantine/core'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React from 'react'
 import { BsThreeDotsVertical } from 'react-icons/bs'
 import { HiArrowLeft } from 'react-icons/hi'
@@ -12,7 +16,20 @@ const formatter = Intl.DateTimeFormat('id-ID', {
   timeZone: 'Asia/Jakarta',
 })
 
-const ClientByIDPage = () => {
+const CounselorByNamePage = () => {
+
+  const router = useRouter();
+
+  const { data } = useQuery(GET_COUNSELOR, {
+    variables: {
+      getCounselorDto: {
+        counselorName: router.query.id as string,
+      },
+    },
+  })
+
+  console.log(data)
+
   return (
     <main className="flex flex-col min-h-screen gap-5 p-5 sm:gap-10 md:px-10 lg:px-20">
       <section>
@@ -39,21 +56,31 @@ const ClientByIDPage = () => {
               <li>Jenjang: S1 Reguler</li>
               <li>Program Studi: Ilmu Komputer</li>
             </ul>
-            <ul className="list-none">
-              <li>Kanal Curhat: Instagram</li>
+            <ul className="flex flex-col gap-2 list-none">
+              <li>Kanal Curhat:</li>
               <li>
-                <div className="p-3 bg-red-100 border-2 border-gray-100 rounded-md">
-                  <h2 className="font-medium">Instagram</h2>
-                  <p className="my-2 text-gray-700">
-                    Jadwal Konseling: Minggu, 08:00 WIB
-                  </p>
+                <div className="p-3 bg-red-100 border-2 border-gray-200 rounded-md">
+                  <h2 className="mb-2 font-medium">Instagram</h2>
                   <a
                     href="http://"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="inline-block px-3 py-2 text-white bg-red-500 rounded"
                   >
-                    Instagram: akmalhakin
+                    Instagram: akmalhakim
+                  </a>
+                </div>
+              </li>
+              <li>
+                <div className="p-3 bg-green-100 border-2 border-gray-200 rounded-md">
+                  <h2 className="mb-2 font-medium">Line</h2>
+                  <a
+                    href="http://"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-block px-3 py-2 text-white bg-green-600 rounded"
+                  >
+                    LINE ID: akmalhakim
                   </a>
                 </div>
               </li>
@@ -63,41 +90,41 @@ const ClientByIDPage = () => {
       </section>
       <section className="">
         <aside className="flex items-center gap-2 mb-3">
-          <h1 className="text-3xl font-semibold">Request Reschedule</h1>
+          <h1 className="text-3xl font-semibold">Daftar Klien</h1>
         </aside>
         <ClientTable
           title=""
-          description=""
+          // description="Berikut merupakan daftar klien konseling Psyhope."
+          description=''
           headerTitle={[
-            'Jadwal Sebelumnya',
-            'Permintaan Jadwal',
-            'Konfirmasi Permintaan',
+            'Nama Klien',
+            'Jadwal Konseling',
+            'Status Request',
           ]}
+          data={[...Array(10)]}
           rowComponent={(val, index) => (
             <tr key={index}>
-              <td className="bg-yellow-50">
-                {formatter.format(new Date()).replace(' pukul', ',')}
+              <td className="min-h-[80px]">
+                <p>Nama panggilan</p>
+                <small className="opacity-70">S1 Reguler Ilmu Komputer</small>
               </td>
-              <td className="bg-blue-50">
-                {formatter.format(new Date()).replace(' pukul', ',')}
+              <td className="min-h-[80px]">
+                <p>
+                  Senin, 08:00
+                </p>
               </td>
-              <td className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                  <button className="px-3 py-2 text-red-700 bg-red-100 rounded-md shadow">
-                    Tolak
-                  </button>
-                  <button className="px-3 py-2 rounded-md shadow bg-primary-50 text-primary-500">
-                    Setujui
-                  </button>
-                </div>
+              <td className="flex items-center justify-between h-full min-h-[80px]">
+                <Badge
+                  color={true ? 'green' : 'red'}
+                >
+                  {true ? 'Accepted' : 'Terminated'}
+                </Badge>
                 <button>
                   <BsThreeDotsVertical />
                 </button>
               </td>
             </tr>
           )}
-          // headerComponent={}
-          data={[...Array(10)]}
         />
       </section>
       <section className="">
@@ -107,13 +134,17 @@ const ClientByIDPage = () => {
         <ClientTable
           title=""
           description=""
-          headerTitle={['Tanggal Konseling', 'Notes']}
+          headerTitle={['Nama Klien', 'Tanggal Konseling', 'Notes']}
           rowComponent={(val, index) => (
             <tr key={index}>
-              <td className="w-1/2">
+              <td>
+                <p>Nama panggilan</p>
+                <small className="opacity-70">S1 Reguler Ilmu Komputer</small>
+              </td>
+              <td className="">
                 {formatter.format(new Date()).replace(' pukul', ',')}
               </td>
-              <td className="flex items-center justify-between w-1/2">
+              <td className="flex items-center justify-between">
                 <div className="flex flex-col gap-2">
                   <p>Judul</p>
                   <p>Notes</p>
@@ -125,97 +156,11 @@ const ClientByIDPage = () => {
             </tr>
           )}
           // headerComponent={}
-          data={[...Array(10)]}
+          data={[...Array(5)]}
         />
-      </section>
-      <section className="">
-        <h1 className="text-3xl font-semibold">Analisis GHQ</h1>
-        <p className="my-3 opacity-70">
-          The GHQ-12 measure has standardized instructions as well as scoring
-          interpretations for the clinician to follow and is administered as a
-          self-report in which the subject is asked to consider 12 questions and
-          how they relate to his or her personal life over the past few weeks.
-          Total scores range from 0 to 36 with a score of 11 or 12 considered
-          typical, scores &gt; 15 suggesting evidence of distress, and scores
-          &gt; 20 are considered severe problems with psychological distress.
-        </p>
-        <ul className="grid grid-cols-1 gap-5 mt-5 list-none sm:grid-cols-2 md:grid-cols-3">
-          <li>
-            <div className="px-4 py-2 text-center text-white rounded-full bg-primary-500 w-max">
-              <h3>Metrics 1</h3>
-            </div>
-            <h4 className="mt-3">Nilai: {8}</h4>
-            <p>
-              Penjelasan:{' '}
-              {
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, inventore voluptatem ipsam optio nostrum, officiis ea pariatur, possimus harum nobis ullam fugit nam necessitatibus ex. Laborum saepe quis minus molestias!'
-              }
-            </p>
-          </li>
-          <li>
-            <div className="px-4 py-2 text-center text-white rounded-full bg-primary-500 w-max">
-              <h3>Metrics 1</h3>
-            </div>
-            <h4 className="mt-3">Nilai: {8}</h4>
-            <p>
-              Penjelasan:{' '}
-              {
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, inventore voluptatem ipsam optio nostrum, officiis ea pariatur, possimus harum nobis ullam fugit nam necessitatibus ex. Laborum saepe quis minus molestias!'
-              }
-            </p>
-          </li>
-          <li>
-            <div className="px-4 py-2 text-center text-white rounded-full bg-primary-500 w-max">
-              <h3>Metrics 1</h3>
-            </div>
-            <h4 className="mt-3">Nilai: {8}</h4>
-            <p>
-              Penjelasan:{' '}
-              {
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, inventore voluptatem ipsam optio nostrum, officiis ea pariatur, possimus harum nobis ullam fugit nam necessitatibus ex. Laborum saepe quis minus molestias!'
-              }
-            </p>
-          </li>
-          <li>
-            <div className="px-4 py-2 text-center text-white rounded-full bg-primary-500 w-max">
-              <h3>Metrics 1</h3>
-            </div>
-            <h4 className="mt-3">Nilai: {8}</h4>
-            <p>
-              Penjelasan:{' '}
-              {
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, inventore voluptatem ipsam optio nostrum, officiis ea pariatur, possimus harum nobis ullam fugit nam necessitatibus ex. Laborum saepe quis minus molestias!'
-              }
-            </p>
-          </li>
-          <li>
-            <div className="px-4 py-2 text-center text-white rounded-full bg-primary-500 w-max">
-              <h3>Metrics 1</h3>
-            </div>
-            <h4 className="mt-3">Nilai: {8}</h4>
-            <p>
-              Penjelasan:{' '}
-              {
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, inventore voluptatem ipsam optio nostrum, officiis ea pariatur, possimus harum nobis ullam fugit nam necessitatibus ex. Laborum saepe quis minus molestias!'
-              }
-            </p>
-          </li>
-          <li>
-            <div className="px-4 py-2 text-center text-white rounded-full bg-primary-500 w-max">
-              <h3>Metrics 1</h3>
-            </div>
-            <h4 className="mt-3">Nilai: {8}</h4>
-            <p>
-              Penjelasan:{' '}
-              {
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, inventore voluptatem ipsam optio nostrum, officiis ea pariatur, possimus harum nobis ullam fugit nam necessitatibus ex. Laborum saepe quis minus molestias!'
-              }
-            </p>
-          </li>
-        </ul>
       </section>
     </main>
   )
 }
 
-export default ClientByIDPage
+export default CounselorByNamePage
