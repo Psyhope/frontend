dc20af2164bfa18bd1351a31eda18054f136ad31
iyoubee <al.ayubi2020@gmail.com>
2023-08-04 10:08:50 +0700

    fix: next config

--- next.config.js
+++ next.config.js
@@ -18,6 +18,7 @@ const nextConfig = {
       'cdn.discordapp.com',
       'upload.wikipedia.org',
       'cdn-icons-png.flaticon.com',
+      'psyhope.s3.ap-northeast-1.amazonaws.com',
     ],
   },
 }
