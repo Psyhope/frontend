6c23eeb9f73f26eefb1893b5032f15b6e42fb10d
iyoubee <al.ayubi2020@gmail.com>
2023-08-10 09:43:20 +0700

    fix: article size

--- src/components/elements/ArticleCard/index.tsx
+++ src/components/elements/ArticleCard/index.tsx
@@ -261,7 +261,7 @@ export const ArticleCard: React.FC<Props> = ({
     <div>
       <NextLink href={`/article/${id}`}>
         <div className=" rounded-lg relative flex-none">
-          <div className="w-full aspect-article relative">
+          <div className="w-full aspect-infografic relative">
             <Image
               alt="thumbnail"
               src={thumbnailUrl}
@@ -406,7 +406,7 @@ export const ArticleCard: React.FC<Props> = ({
               className="text-black font-inter font-normal md:text-base text-sm pb-2"
               required
             >
-              Upload Thumbnail (3:2)
+              Upload Thumbnail (4:5)
             </Input.Label>
             <FileInput
               placeholder="Click here to pick file"
@@ -421,7 +421,7 @@ export const ArticleCard: React.FC<Props> = ({
                 breakpoints={[{ maxWidth: 'sm', cols: 1 }]}
                 className="mt-5"
               >
-                <div className="w-full md:w-1/4 aspect-article relative">
+                <div className="w-full md:w-1/4 aspect-infografic relative">
                   <Image
                     src={previewThumbnail}
                     fill
