59aa234ab03f1d64c038f9e505dfe2076f17b830
iyoubee <al.ayubi2020@gmail.com>
2023-08-17 09:41:07 +0700

    feat: add footer assets

--- src/components/elements/Footer/index.tsx
+++ src/components/elements/Footer/index.tsx
@@ -1,13 +1,16 @@
 'use client'
-import { Bismit, Psyhope } from '@icons'
+import { Bismit, FooterAsset1, FooterAsset2, Psyhope } from '@icons'
 import React from 'react'
 
 export const Footer: React.FC = () => {
   return (
     <>
-      <div className="w-full bg-[#6941C6] flex flex-col items-center text-white py-10">
-        <p className="font-bold font-inter text-3xl mb-6">Made With 💖</p>
-        <div className="flex flex-col md:flex-row md:gap-20 gap-5">
+      <div className="w-full bg-[#6941C6] flex flex-col items-center text-white py-10 relative overflow-hidden">
+        <FooterAsset1 className="absolute top-0 right-0 hidden lg:block" />
+        <FooterAsset2 className="absolute bottom-0 left-0" />
+        <FooterAsset2 className="absolute top-0 right-0 rotate-180 lg:hidden" />
+        <p className="font-bold font-inter text-3xl mb-6 z-10">Made With 💖</p>
+        <div className="flex flex-col md:flex-row md:gap-20 gap-5 z-10">
           <div>
             <p className="font-semibold text-xl mb-4">Crafted by:</p>
             <div className="bg-white rounded-lg p-[19px]">
